b95563d37d436c4272dbfd0100217fb062b4b89e
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-13 03:14:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
-# Write your methods here
+def loop_message_five_times(message)
+  message = "Hello World.\n"
+  
+  puts message * 5
+end
+
+def loop_message_n_times(message, number)
+  message = "Hello Moon.\n"
+  number = "5"
+  
+  puts "#{message} to be output #{number} times."
+end
