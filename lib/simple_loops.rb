05dab1540a2dffe5a05dec819e9edec3f9df943a
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-13 03:27:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,6 +8,6 @@ def loop_message_n_times(message, number)
   message = "Hello Moon."
   number = "5"
   
-  puts "\n'#{message}' to be output #{number} times" * 5
+  puts "\nExpected '#{message}' to be output #{number} times" * 5
 
 end
