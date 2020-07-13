fd0adc63b2df8797f8491f22435d93e6c7016649
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-13 03:49:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,8 +5,8 @@ def loop_message_five_times(message)
 end
 
 def loop_message_n_times(message, number)
-  message = "Hello Moon."
+  message = "\nHello Moon."
   number = 5
- puts "\n#{message} #{number}" * 5
+ puts "#{message} #{number}" * 5
 
 end
